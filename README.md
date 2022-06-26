7c08c7a682ccda4749d486bd5bc8f283b4fb357f
ZJAllenJiang <weijiang@bu.edu>
2022-06-26 21:55:57 +0800

    Initialize SpringBoot integration with Redis on session management

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Integration-Deployment
+
+## Kubernetes
+### Kubernetes Dashboard
+1. Dashboard login: kubectl -n kubernetes-dashboard get secret $(kubectl -n kubernetes-dashboard get sa/admin-user -o jsonpath="{.secrets[0].name}") -o go-template="{{.data.token | base64decode}}"
---
+++ src/main/java/org/allen/controller/SessionTestController.java
@@ -0,0 +1,38 @@
+package org.allen.controller;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SessionTestController
+{
+    private static final Logger LOGGER = LoggerFactory.getLogger(SessionTestController.class);
+
+    @GetMapping("/hello")
+    public String hello()
+    {
+        return "hello!";
+    }
+
+    @GetMapping("/login")
+    public String login(final HttpSession session)
+    {
+        LOGGER.info("logged with session id:{}", session.getId());
+        return "logged with session id: " + session.getId();
+    }
+
+    @GetMapping("/logout")
+    public void logout(final HttpServletResponse response, final HttpSession session) throws IOException
+    {
+        LOGGER.info("Logout with session id:{}", session.getId());
+        session.invalidate();
+        response.sendRedirect("/login");
+    }
+}
