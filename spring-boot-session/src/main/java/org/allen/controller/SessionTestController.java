e23cd85af6ecb20a3a95a0726d0b3b0590ca1de1
ZJAllenJiang <weijiang@bu.edu>
2022-07-03 20:53:05 +0800

    Adjust project directory

--- spring-boot-session/src/main/java/org/allen/controller/SessionTestController.java
+++ spring-boot-session/src/main/java/org/allen/controller/SessionTestController.java
@@ -1,14 +1,13 @@
 package org.allen.controller;
 
-import java.io.IOException;
-
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
 @RestController
 public class SessionTestController
